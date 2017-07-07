bc0ef07a893c36523fe2e0b122e023a313a8022b
zachlandes <zlandes@Zacharys-MacBook-Pro.local>
2017-07-07 15:26:42 -0700

    Added local mp3 playback and more

--- basic_bot.py
+++ basic_bot.py
@@ -1,34 +1,73 @@
 import discord
 import asyncio
+from discord.ext import commands
 
-token = ''
+bot  = commands.Bot('!')
 
-with open('keys') as keys:
-    for i in keys:
-        token = i
-
-client = discord.Client()
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-@client.event
+@bot.event
+async def on_member_join(member):
+    return await bot.send_message(member.server.default_channel, 'Welcome to {} !'.format(member.server.name))
+
+
+"""
+@bot.event
 async def on_message(message):
     if message.content.startswith('!test'):
         counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
+        tmp = await bot.send_message(message.channel, 'Calculating messages...')
+        async for log in bot.logs_from(message.channel, limit=100):
             if log.author == message.author:
                 counter += 1
 
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+        await bot.edit_message(tmp, 'You have {} messages.'.format(counter))
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+        await bot.send_message(message.channel, 'Done sleeping')
+"""
 
-print(token)
-client.run(token)
+@bot.command(pass_context = True)
+async def ping(ctx):
+    await bot.say("Pong!")
+
+@bot.command(pass_context = True)
+async def join(ctx):
+    try:
+        voice_channel = ctx.message.author.voice_channel
+        await bot.join_voice_channel(voice_channel)
+    except:
+        my_embed = discord.Embed(description =  "Can't <join> - you're not in a voice channel!", color = 0xff0000) #color must be hex, or discord.Color.?
+        return await bot.say(embed=my_embed)
+
+@bot.command(pass_context = True)
+async def leave(ctx):
+    for c in bot.voice_clients:
+        if(c.server == ctx.message.server):
+            return await c.disconnect()
+
+"""
+embed = discord.Embed(title = "Some title", color = 0x00FF00) #color must be hex, or discord.Color.?
+embed.add_field(name="A field", value="A value")
+embed.add_field("Another field", "Another value for the field") #Can add more fields
+
+return await client.say(embed = embed)
+"""
+
+#h/t Necro of Necrobot
+@bot.command(pass_context = True)
+async def play(ctx, filename):
+    vc = ctx.message.author.voice_channel
+    voice_client = await bot.join_voice_channel(vc)
+    player = voice_client.create_ffmpeg_player(filename, after=lambda:close_player(filename))    
+    #await bot.say(":musical_note: | Playing `" + player.title)
+
+    player.start()
+
+
+bot.run('MzI5NzY3ODQ1NzY2NjI3MzI4.DD7LKQ.7FoZd6R8CAOZdy8KKTMyS3EQB6g')
